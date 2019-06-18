36ddcddce35ed3bd0cc27e6c52eda1fece39bd17
Jeho0216 <leejen2003@gmail.com>
2019-06-18 11:28:16 +0900

    create project files.

---
+++ attendance_manage/mainwindow_dashboard.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow_dashboard.h"
+#include "ui_mainwindow_dashboard.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ attendance_manage/mainwindow_dashboard.h
@@ -0,0 +1,22 @@
+#ifndef MAINWINDOW_DASHBOARD_H
+#define MAINWINDOW_DASHBOARD_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_DASHBOARD_H
